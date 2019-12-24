1b6294483029593bdd536ee79613bf101899e946
chan <rkdcks020328@gmail.com>
2019-12-24 11:30:00 +0900

    기본틀

--- NetworkProject/Form1.Designer.cs
+++ NetworkProject/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(685, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "보내기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(553, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 46);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(553, 280);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- NetworkProject/Form1.cs
+++ NetworkProject/Form1.cs
@@ -15,6 +15,25 @@ namespace NetworkProject
         public Form1()
         {
             InitializeComponent();
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 메세지 전송
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // 보낼 메세지를 입력하는 textbox
+            String msg = this.textBox1.Text; 
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // 주고받은 채팅을 보여주는 textbox
+            this.textBox3.ReadOnly = true;
+
         }
     }
 }
